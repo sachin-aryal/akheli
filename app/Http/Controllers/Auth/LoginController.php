f760c078ee68f902784ffdd1820a7044087c8749
Sachin Aryal <saaryal51@gmail.com>
2017-08-20 23:02:18 +0545

    Disabled user will not be able to login.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use APP\User;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,40 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function credentials($request)
+    {
+        $credentials = $request->only($this->username(), 'password');
+
+        return array_add($credentials, 'enabled', '1');
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $user = User::where('email', '=', $request->email)->get();
+        if($user->isNotEmpty()){
+            if(!$user->first()->enabled){
+                $errors = [$this->username() => "User is disabled."];
+            }else{
+                $errors = [$this->username() => trans('auth.failed')];
+            }
+        }else{
+            $errors = [$this->username() => trans('auth.failed')];
+        }
+
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
     }
 }
