c2b1b48892803750ef08903472ab6038b9f8a291
Pratik Budhathoki <pratik.budhathoki@deerwalk.edu.p>
2017-08-24 01:08:50 +0545

    deisgn login page

--- user/login.php
+++ user/login.php
@@ -11,16 +11,49 @@ redirectIfLoggedIn("../index.php")
 ?>
 <html>
 <head>
-<title>Login - Akheli</title>
+    <title>Login - Akheli</title>
+    <link href="https://fonts.googleapis.com/css?family=Cagliostro|Open+Sans+Condensed:300" rel="stylesheet">
+
+    <link rel="stylesheet" href="../public/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
-<?php include "../shared/_header.php"?>
-<form method="post" action="../controller/user.php">
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username"/>
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password"/>
-    <input type="submit" name="login" value="Login"/>
-</form>
+<div class="login-wrapper">
+    <img src="../public/img/login-back.png" alt="">
+
+
+    <div class="absolute-center">
+        <div class="col-md-6 no-padding">
+            <img src="../public/img/ecommerce.jpg" alt="">
+        </div>
+        <div class="col-md-6 clearfix">
+            <div class="absolute-center">
+            <h2 class="title">Akheli</h2>
+            <form method="post" action="../controller/user.php">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <div>
+                        <input class="form-control" type="text" name="username" id="username"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <div>
+                        <input class="form-control" type="password" name="password" id="password"/>
+                    </div>
+                </div>
+                <input class="btn btn-view btn-block" type="submit" name="login" value="Login"/>
+            </form>
+                <div class="pull-left">
+                    <a href="#">Forgot Password</a>
+                </div>
+                <div class="pull-right">
+                    <a href="#">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
