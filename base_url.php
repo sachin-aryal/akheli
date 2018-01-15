88944391f1161565f9382b793c8877f9c39aea50
Sachin Aryal <saaryal51@gmail.com>
2018-01-15 09:43:37 +0545

    Work Completed with partial testing.

--- base_url.php
+++ base_url.php
@@ -1,5 +1,5 @@
 <?php
-define("BASE_URL","http://localhost/~sachin/akheli/");
-//define("BASE_URL","http://akhely.com/");
+//define("BASE_URL","http://localhost/~sachin/akheli/");
+define("BASE_URL","http://akhely.com/");
 define("PROJECT_PATH",__DIR__);
 ?>
